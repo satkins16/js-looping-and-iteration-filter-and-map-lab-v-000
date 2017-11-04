5b928eff1d3b8e465de7b2e045155648f6b65ab2
Scott Atkins <satkins16@gmail.com>
2017-11-04 00:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function(driver) { return driver.revenue > revenue })
 }
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function(driver) { driver.name })
+}

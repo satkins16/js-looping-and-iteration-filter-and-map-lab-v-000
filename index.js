d2bfbe56c2f08127929f0948e2f1ea3b20e7fc18
Scott Atkins <satkins16@gmail.com>
2017-11-04 00:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function exactMatch(drivers, attribute) {
 }
 
 function exactMatchToList(drivers, attribute) {
-  exactMatch(drivers, attributes)
+  exactMatch(drivers, attributes).map(function(driver) {return driver.name})
 }
